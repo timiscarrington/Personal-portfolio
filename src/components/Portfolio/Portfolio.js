2366a25527ad6e2653dfaa610aa2415a1e0c7c0a
TimisCarrington <timiscarrington@gmail.com>
2023-02-22 10:50:01 -0800

    updated with new project(reddit clone) also changed the names of the projects

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,68 +5,70 @@ import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import portfolioData from '../../data/portfolio.json'
 
 const Portfolio = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    console.log(portfolioData)
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
+  console.log(portfolioData)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
 
-        return () => {
-            clearTimeout(timer)
-
-        }
-    });
-
-    const renderPortfolio = (portfolioData) => {
-        return (
-            <div className='images-container'>
-                {
-                    portfolioData.map((project, idx) => {
-                        return (
-                            <div className='image-box' key={idx}>
-                                <img
-                                    src={project.cover}
-                                    className='portfolio-image'
-                                    alt='project'
-                                />
-                                <div className='content'>
-                                    <p className='title'>{project.title}</p>
-                                    <h4 className='description'>{project.description}</h4>
-                                    <div className='btn-container'>
-                                        <button
-                                            className='btn'
-                                            onClick={() => window.open(project.URL)}>Live Site</button>
-                                        <button
-                                            className='btn'
-                                            onClick={() => window.open(project.Github_Frontend)}>GitHub</button>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-    
-                    })
-                }
-            </div>
-        )
+    return () => {
+      clearTimeout(timer)
     }
+  })
 
+  const renderPortfolio = (portfolioData) => {
     return (
-        <>
-            <div className='container portfolio-page'>
-                <h1 className='page-title'>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={'Portfolio'.split("")}
-                        idx={15}
-                    />
-                </h1>
-                <div>{renderPortfolio(portfolioData.projects)}</div>
+      <div className="images-container">
+        {portfolioData.map((project, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={project.cover}
+                className="portfolio-image"
+                alt="project"
+              />
+              <div className="content">
+                <p className="title">{project.title}</p>
+                <h4 className="description">{project.description}</h4>
+                <div className="btn-container">
+                  <button
+                    className="btn"
+                    onClick={() => window.open(project.URL)}
+                  >
+                    Live Site
+                  </button>
+                  <button
+                    className="btn"
+                    onClick={() => window.open(project.Github_Frontend)}
+                  >
+                    GitHub
+                  </button>
+                </div>
+              </div>
             </div>
-            <Loader type='pacman' />
-        </>
+          )
+        })}
+      </div>
     )
+  }
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'Portfolio'.split('')}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio(portfolioData.projects)}</div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
-export default Portfolio;
+export default Portfolio
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,14 +3,19 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoT from '../../assets/images/My project-1 (2).png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faClose, faEnvelope, faHome, faProjectDiagram, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBars,
+  faClose,
+  faEnvelope,
+  faHome,
+  faProjectDiagram,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { useState } from 'react'
 
-
 const Sidebar = () => {
-  const [showNav, setShowNav] = useState(false);
-
+  const [showNav, setShowNav] = useState(false)
 
   return (
     <div className="nav-bar">
@@ -18,16 +23,39 @@ const Sidebar = () => {
         <img src={LogoT} alt="logo" />
       </Link>
       <nav className={showNav ? 'mobile-show' : ''}>
-        <NavLink onClick={() => setShowNav(false)} exact='true' activeclassname="active" to="/" >
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          to="/"
+        >
           <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
         </NavLink>
-        <NavLink onClick={() => setShowNav(false)} exact='true' activeclassname='active' className="about-link" to="/about" >
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
           <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
         </NavLink>
-        <NavLink onClick={() => setShowNav(false)} exact='true' activeclassname="active" className="portfolio-link" to="/portfolio" >
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+        >
           <FontAwesomeIcon icon={faProjectDiagram} color="#4d4d4e" />
         </NavLink>
-        <NavLink onClick={() => setShowNav(false)} exact='true' activeclassname="active" className="contact-link" to="/contact" >
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
         <FontAwesomeIcon
@@ -35,7 +63,8 @@ const Sidebar = () => {
           icon={faClose}
           color="#D5AD36"
           size="3x"
-          className='close-icon' />
+          className="close-icon"
+        />
       </nav>
       <ul>
         <li>
@@ -68,12 +97,12 @@ const Sidebar = () => {
       <FontAwesomeIcon
         onClick={() => setShowNav(true)}
         icon={faBars}
-        color='#D5AD36'
-        size='3x'
-        className='hamburger-icon'
+        color="#D5AD36"
+        size="3x"
+        className="hamburger-icon"
       />
     </div>
   )
 }
 
-export default Sidebar;
+export default Sidebar
