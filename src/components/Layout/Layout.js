9e5dbc98964e950dfecb273aceba3fcfde0fd283
TimisCarrington <timiscarrington@gmail.com>
2023-03-01 11:08:32 -0800

    Revert "modified some of the layout styling and removed the html tags, i was using for styling"
    This reverts commit acb50c6a569eff47adf57a7e8f73b4381b9f2e5d.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,17 +7,17 @@ const Layout = () => {
   return (
     <div className='App'>
       <Sidebar />
-      {/* <div className='page'>
-        <span className='tags top-tags'>&lt;body&gt;</span> */}
+      <div className='page'>
+        <span className='tags top-tags'>&lt;body&gt;</span>
 
         <Outlet />
 
-        {/* <span className='tags bottom-tags'>
-          &lt;body&gt;
+        <span className='tags bottom-tags'>
+          &lt;body&gt
           <br />
           <span className='bottom-tag-html'>&lt;html&gt;</span>
         </span>
-      </div> */}
+      </div>
     </div>
   )
 }
